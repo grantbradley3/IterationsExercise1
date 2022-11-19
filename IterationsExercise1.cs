758d720f0c38480b3586bfa940e326444ed552a1
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-18 20:24:40 -0500

    initial commit

---
+++ IterationsExercise1.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0;) i < 15; int++)
+    {
+            Console.WriteLine($"{i});
+}    }
+}
